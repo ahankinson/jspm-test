fe289ffe2cb4422565348c810c0a6b5860c68e40
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-02-28 22:57:45 -0500

    Initial Commit

---
+++ config.js
@@ -0,0 +1,45 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "babel",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "backbone": "npm:backbone@1.1.2",
+    "backbone.marionette": "npm:backbone.marionette@2.4.0",
+    "jquery": "npm:jquery@2.1.3",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.0"
+    },
+    "npm:backbone.babysitter@0.1.6": {
+      "backbone": "npm:backbone@1.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "underscore": "npm:underscore@1.6.0"
+    },
+    "npm:backbone.marionette@2.4.0": {
+      "backbone": "npm:backbone@1.1.2",
+      "backbone.babysitter": "npm:backbone.babysitter@0.1.6",
+      "backbone.wreqr": "npm:backbone.wreqr@1.3.1",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "underscore": "npm:underscore@1.6.0"
+    },
+    "npm:backbone.wreqr@1.3.1": {
+      "backbone": "npm:backbone@1.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "underscore": "npm:underscore@1.6.0"
+    },
+    "npm:backbone@1.1.2": {
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "underscore": "npm:underscore@1.6.0"
+    },
+    "npm:jquery@2.1.3": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
+
---
+++ main.js
@@ -0,0 +1,4 @@
+import $ from 'jquery';
+import Backbone from 'backbone';
+Backbone.$ = $;
+import Marionette from 'backbone.marionette';
