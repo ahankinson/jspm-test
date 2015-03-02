ecc821adfaa807338b284aca6eb46fff328b2250
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-03-02 11:03:56 -0500

    Still not working...

--- config.js
+++ config.js
@@ -1,17 +1,21 @@
 System.config({
   "baseURL": "/",
-  "transpiler": "babel",
   "paths": {
     "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
+    "npm:*": "jspm_packages/npm/*.js",
+    "github:*": "jspm_packages/github/*.js"
+  },
+  "shim": {
+    "npm:backbone@1.1.2": {
+      "deps": ["jquery", "underscore"]
+    }
   }
 });
 
 System.config({
   "map": {
     "backbone": "npm:backbone@1.1.2",
-    "backbone.marionette": "npm:backbone.marionette@2.4.0",
+    "backbone.marionette": "npm:backbone.marionette@2.4.1",
     "jquery": "npm:jquery@2.1.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.0"
@@ -21,11 +25,10 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "underscore": "npm:underscore@1.6.0"
     },
-    "npm:backbone.marionette@2.4.0": {
+    "npm:backbone.marionette@2.4.1": {
       "backbone": "npm:backbone@1.1.2",
       "backbone.babysitter": "npm:backbone.babysitter@0.1.6",
       "backbone.wreqr": "npm:backbone.wreqr@1.3.1",
-      "jquery": "npm:jquery@2.1.3",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "underscore": "npm:underscore@1.6.0"
     },
@@ -35,6 +38,7 @@ System.config({
       "underscore": "npm:underscore@1.6.0"
     },
     "npm:backbone@1.1.2": {
+      "process": "github:jspm/nodelibs-process@0.1.1",
       "underscore": "npm:underscore@1.6.0"
     },
     "npm:jquery@2.1.3": {
--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 import $ from 'jquery';
 import Backbone from 'backbone';
-Backbone.$ = $;
 import Marionette from 'backbone.marionette';
+
+// var App = new Marionette.Application();
+
+// App.on('start', function() {
+//   'use strict';
+//   console.log('Hello World');
+// });
+
+// App.start();
